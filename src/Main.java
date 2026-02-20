41ef9061b2320e8ad501e2cd84029bd46cf551c1
Pedro Rocha <pedrohenriquecamposrocha5@gmail.com>
2026-02-20 00:45:09 -0300

    Estrutura inicial do encurtador com menu e HashMap

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+        while (entradaLink != "") {
+            Scanner scanner = new Scanner(System.in);
+            String entradaLink = scanner.nextLine();
+            int opcao = scanner.nextInt();
+            System.out.println("Bem vindo ao Encurtador de Links !");
+            System.out.println("Digite a opção 1 para encurtar ");
+            System.out.println("Digite a opção 2 para consultar ");
+            System.out.println("Digite a opção 3 para sair");
+
+            if (opcao == 1) {
+
+                else if (opcao == 2){
+
+                    else if{
+
+                        System.out.println("Saindo do programa");
+
+                        else{
+
+                            System.out.println("Opção inválida. Programa encerrado");
+                        }
+                    }
+
+                }
+
+
+            }
+
+        }
+
+        System.out.println("Programa encerrado");
+
+    }
+}
