1e11abc4ba796b5a28b2eb0b4936a76432541b81
Pedro Rocha <pedrohenriquecamposrocha5@gmail.com>
2026-02-22 19:35:43 -0300

    Menu funcional

--- src/Main.java
+++ src/Main.java
@@ -4,37 +4,34 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
 
-        while (entradaLink != "") {
-            Scanner scanner = new Scanner(System.in);
-            String entradaLink = scanner.nextLine();
-            int opcao = scanner.nextInt();
+
+        while (true) {
+
             System.out.println("Bem vindo ao Encurtador de Links !");
             System.out.println("Digite a opção 1 para encurtar ");
             System.out.println("Digite a opção 2 para consultar ");
             System.out.println("Digite a opção 3 para sair");
+            System.out.println("----------------------------------------");
+            System.out.println("Digite a opção: ");
+
+            Scanner scanner = new Scanner(System.in);
+            int opcao = scanner.nextInt();
+
 
             if (opcao == 1) {
 
-                else if (opcao == 2){
-
-                    else if{
-
-                        System.out.println("Saindo do programa");
-
-                        else{
-
-                            System.out.println("Opção inválida. Programa encerrado");
-                        }
-                    }
-
-                }
-
+            }
+            else if (opcao == 2) {
 
             }
+            else if (opcao == 3) {
 
+                break;
+
+            }
+            else {
+                System.out.println("Opção Inválida");
+            }
         }
-
-        System.out.println("Programa encerrado");
-
     }
 }
