250c94d0d14a1c08dd732822ba840d156b4895be
Pedro Rocha <pedrohenriquecamposrocha5@gmail.com>
2026-02-23 01:03:51 -0300

    Primeira opcao funcionando

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,42 @@
+import java.awt.*;
 import java.util.Scanner;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        HashMap<String, String> links = new HashMap<>(); // Nosso "banco de dados" em memória
+        int contador = 1; //contador para o loop de links. Vão ser gerado vários códigos
+
 
         while (true) {
 
-            System.out.println("Bem vindo ao Encurtador de Links !");
-            System.out.println("Digite a opção 1 para encurtar ");
-            System.out.println("Digite a opção 2 para consultar ");
-            System.out.println("Digite a opção 3 para sair");
-            System.out.println("----------------------------------------");
-            System.out.println("Digite a opção: ");
+            System.out.println("""
+                               === Encurtador de Links ===
+                               1 - Encurtar link
+                               2 - Consultar link
+                               3 - Sair
+                               ---------------------------
+                               Digite a opção:
+                               """);
 
-            Scanner scanner = new Scanner(System.in);
             int opcao = scanner.nextInt();
-
+            scanner.nextLine(); /*limpa o ENTER - O Java lê apenas o número, mas não consome o ENTER que você apertou =
+                                   Ok Java, agora ignore o ENTER que ficou aí.*/
 
             if (opcao == 1) {
 
+                System.out.println("Digite a URL");
+                String entradaUrl = scanner.nextLine();
+
+                String codigo = "L" + contador;
+
+                links.put(codigo,entradaUrl);
+
+                System.out.println("O código é: " + codigo);
+
+                contador++;
             }
             else if (opcao == 2) {
 
